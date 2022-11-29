26d3ce600393d26f9c7dcebf9920be1ad1ffcc33
NARANDORJ BYAMBADORJ <NARANDORJ.BYAMBADORJ@GMAIL.COM>
2022-11-29 19:47:54 +0800

    20221129 home

---
+++ operations/tip/main.js
@@ -0,0 +1,3 @@
+let hoolnii_une = prompt("hoolnii uniig oruulna uu");
+let message = hoolnii_une >= 5000 && hoolnii_une <= 30000 ? hoolnii_une * 1.15 : hoolnii_une * 1.15;
+console.log("tanii niit toloh mongo hool + tip ", message);
