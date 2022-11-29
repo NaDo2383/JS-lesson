5b2481815ffac0d34e8be06af9cd399fbd180cab
NARANDORJ BYAMBADORJ <NARANDORJ.BYAMBADORJ@GMAIL.COM>
2022-11-29 18:42:25 +0800

    20221129home

---
+++ operations/huvisagch/main.js
@@ -0,0 +1,39 @@
+//task1
+console.log("task1");
+let a = 85, b = 12, c = 45, d = 99, n = 0;
+console.log("ogogdson toonuud", a, b, c, d);
+if (a >= 80) {
+    n = n + a;
+}
+if (b >= 80) {
+    n = n + b;
+}
+if (c >= 80) {
+    n = n + c;
+}
+if (d >= 80) {
+    n = n + d;
+}
+console.log("80 aas ih toonuudiig niilber ", n);
+
+//task2
+console.log("task2");
+let a1 = 2, b1 = 3, c1 = 45, d1 = 71, n1 = 1;
+console.log("ogogdson toonuud", a1, b1, c1, d1);
+if (a1 <= 5) {
+    n1 = n1 * a1;
+}
+if (b1 <= 5) {
+    n1 = n1 * b1;
+}
+if (c1 <= 5) {
+    n1 = n1 * c1;
+}
+if (d1 <= 5) {
+    n1 = n1 * d1;
+}
+if (n1 > 1) {
+    console.log("5 aas baga toonuudin urjver ", n1);
+} else {
+    console.log("5 aas baga too ogogdoogui bn");
+}
