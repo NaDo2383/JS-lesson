f82e01d5ccb1dea342fc664ee681c763d9d9c208
NaDo2383 <narandorj.byambadorj@gmail.com>
2022-12-01 09:46:39 +0800

    20221201

--- JS numbers and string/RD/main.js
+++ JS numbers and string/RD/main.js
@@ -1,6 +1,6 @@
 let rd = prompt("ta RD aa oruulna uu");
 if (rd.length == 10) {
-    if (Number(rd[0]) != NaN && Number(rd[1]) != NaN) {
+    if (Number.isInteger(Number(rd[0])) == false && Number.isInteger(Number(rd[1]))) {
         if (Number(rd.substring(2)) != NaN) {
             console.log("та монгол улсын регистрийн дугаарыг зөв орууллаа");
         } else {
